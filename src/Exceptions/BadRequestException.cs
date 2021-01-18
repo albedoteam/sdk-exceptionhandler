f2b91616a71e83bf534597d0180c4b38f8e6e295
fiduta <eryckson@gmail.com>
2021-01-18 20:02:57 -0300

    new exceptions

---
+++ src/Exceptions/BadRequestException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AlbedoTeam.Sdk.ExceptionHandler.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException()
+        {
+        }
+
+        public BadRequestException(string message)
+            : base(message)
+        {
+        }
+
+        public BadRequestException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
